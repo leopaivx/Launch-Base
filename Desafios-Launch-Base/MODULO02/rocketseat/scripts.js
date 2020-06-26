1e567aff1e4098f604de0d80d284d3954c052995
Leonardo Paiva <leonardo_ppaiva@outlook.com>
2020-06-26 20:29:15 -0300

    content

---
+++ Desafios-Launch-Base/MODULO02/rocketseat/scripts.js
@@ -0,0 +1,29 @@
+const modalOverlay = document.querySelector('.modalOverlay')
+
+const cards = document.querySelectorAll('.card')
+
+const modal = document.querySelector('.modal')
+
+for(let card of cards){
+    const rocketId = card.getAttribute('id')
+    card.addEventListener('click', function(){
+        modalOverlay.classList.add('active')
+        modal.querySelector('iframe').src = `https://rocketseat.com.br/${rocketId}`
+    })
+}
+
+
+
+
+document.querySelector('.close-modal').addEventListener('click', function(){
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ""
+})
+
+document.querySelector('.maximize-modal').addEventListener('click', function(){
+    if(modal.classList.contains('maximized')){
+        modal.classList.remove('maximized')
+    }else{
+        modal.classList.add('maximized')
+    }
+})
