f029959d1c64ea17a49931b8543aeb5ff5ed04d6
Leonardo Paiva <leonardo_ppaiva@outlook.com>
2020-07-31 17:49:10 -0300

    editing data

--- Desafios-Launch-Base/MODULO04/teachers.js
+++ Desafios-Launch-Base/MODULO04/teachers.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
-const {age, graduation} = require('./utils')
+const {age, graduation, date} = require('./utils')
 
 
 //create
@@ -60,4 +60,25 @@ exports.show = function(req,res){
     }
 
     return res.render('teachers/show',{teacher})
+}
+
+//edit
+exports.edit = function(req,res){
+    const {id} = req.params
+
+    const foundTeacher = data.teachers.find(function(teacher){
+        return teacher.id == id
+    })
+
+    if(!foundTeacher){
+        return res.send("Teacher not found")
+    }
+
+    const teacher = {
+        ...foundTeacher,
+        date: date(foundTeacher.birth)
+    }
+
+
+    return res.render("teachers/edit", {teacher})
 }
--- Desafios-Launch-Base/MODULO04/utils.js
+++ Desafios-Launch-Base/MODULO04/utils.js
@@ -29,7 +29,23 @@ module.exports = {
         }
 
         return graduation
-    }
+    },
+    
+    date: function(timestamp){
+        const date = new Date(timestamp)
+
+     
+        const year = date.getUTCFullYear()
+
+   
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+
+     
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+       
+        return `${year}-${month}-${day}`
+    }      
 
 
 }  
